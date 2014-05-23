eb5e21d78bea98b0116d842b1cf852e96dfff3f5
iky <iky@zuhlke.com>
2014-05-23 22:34:14 +0100

    demo web app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ikyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'Apples',
+      'Bananas',
+      'Pears'
+    ];
+  });
